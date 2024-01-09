71577bc8d766c14e7666560aba02725e6be93a55
Youssef Tawil <yousseftawil321@gmail.com>
2024-01-09 10:35:25 -0800

    playerSelection and computerSelection working

---
+++ scripts.js
@@ -0,0 +1,19 @@
+
+const computerSelection = getComputerChoice();
+const playerSelection = prompt("Rock, paper, or scissors?").toLowerCase();
+
+
+function getComputerChoice () {
+    const choices = ["rock", "paper", "scissors"];
+    const choice = Math.floor(Math.random() * choices.length);
+    return(choice, choices[choice]);
+}
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+  }
+     
+  /*console.log(playRound(playerSelection, computerSelection));*/
+
+  function game() {
+
+  }

5cf0203f2cf6a610cdf1fcc3e40c5630f3983f85
Youssef Tawil <yousseftawil321@gmail.com>
2024-01-09 20:56:14 -0800

    completed game in console.

--- scripts.js
+++ scripts.js
@@ -1,10 +1,9 @@
 var computerSelection = getComputerChoice();
-var playerSelection = getPlayerSelection();
+var playerSelection = " ";
 
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     const choice = Math.floor(Math.random() * 3);
-    console.log(choice);
     return (choice, choices[choice]);
 }
 
@@ -21,49 +20,32 @@ function getPlayerSelection() {
 }
 
 var playerScore = 0;
-var playerWin = Boolean();
 const playerWon = (`${playerSelection} beats ${computerSelection}, point to player!
 Current score: You: ${playerScore} Computer: ${computerScore}`);
 var computerScore = 0;
-var computerWin = Boolean();
 const computerWon = (`${computerSelection} beats ${playerSelection}, point to computer!
 Current score: You: ${playerScore} Computer: ${computerScore}`);
 
 function game() {
-    while ((playerScore || computerScore < 5)) {
+    while ((playerScore || computerScore) < 5) {
+        playerSelection = getPlayerSelection();
+        computerSelection = getComputerChoice();
         playRound(playerSelection, computerSelection);
-        console.log(playerSelection);
-        console.log(computerSelection);
-        if (playerWin == true) {
-            playerScore = playerScore + 1;
-            console.log(playerScore);
-            playerWin = " ";
-            computerWin = " ";
-            console.log(`${playerSelection} beats ${computerSelection}, point to player!
-            Current score: You: ${playerScore} Computer: ${computerScore}`);
-        } else if (computerWin == true) {
-            computerScore = computerScore + 1;
-            console.log(computerScore);
-            playerWin = " ";
-            computerWin = " ";
-            console.log(`${computerSelection} beats ${playerSelection}, point to computer!
-            Current score: You: ${playerScore} Computer: ${computerScore}`);
+        if (playerScore === 5) {
+            console.log("Good job, you have beat the computer!");
+            break;
+        } else if (computerScore === 5) {
+            console.log("Tough luck next time, the computer beat you...");
+            break;
         }
     } 
-    if (playerScore === 5) {
-        i += 1;
-        console.log("Good job, you have beat the computer!");
-    } else if (computerScore === 5) {
-        i += 1;
-        console.log("Tough luck next time, the computer beat you...");
-    }
+    
 }
 
 
 console.log(game(playerSelection, computerSelection));
 
 function playRound() {
-
     while (playerSelection === computerSelection) {
         console.log("It's a tie! Go again.");
         playerSelection = getPlayerSelection();
@@ -72,14 +54,16 @@ function playRound() {
     if ((playerSelection === "rock" && computerSelection === "paper") ||
         (playerSelection === "paper" && computerSelection === "scissors") ||
         (playerSelection === "scissors" && computerSelection === "rock")) {
-        computerWin = true;
-        playerWin = false;
+        computerScore += 1;
+        console.log(`${computerSelection} beats ${playerSelection}, point to computer!
+        Current score: You: ${playerScore} Computer: ${computerScore}`);
         return;
     } else if ((playerSelection === "rock" && computerSelection === "scissors") ||
         (playerSelection === "paper" && computerSelection === "rock") ||
         (playerSelection === "scissors" && computerSelection === "paper")) {
-            computerWin = false;
-            playerWin = true;
+            playerScore += 1;
+            console.log(`${playerSelection} beats ${computerSelection}, point to player!
+            Current score: You: ${playerScore} Computer: ${computerScore}`);
             return;
     }
 }

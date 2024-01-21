9c262052b6eba2f8e180e6020db0303aa278deba
Youssef Tawil <yousseftawil321@gmail.com>
2024-01-20 16:44:14 -0800

    press buttons for input outputs game to console

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 var computerSelection = getComputerChoice();
-var playerSelection = " ";
+let playerSelection = " ";
 
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
@@ -7,64 +7,47 @@ function getComputerChoice() {
     return (choice, choices[choice]);
 }
 
-function getPlayerSelection() {
-    for (let i = 0; i = 1; i) {
-        playerInput = prompt("Rock, paper, or scissors?").toLowerCase();
-        if (!((playerInput == "rock") || (playerInput == "scissors") || (playerInput == "paper"))) {
-            console.log("Enter rock, paper, or scissors...");
-        } else if ((playerInput == "rock") || (playerInput == "scissors") || (playerInput == "paper")) {
-            return (playerInput);
-        }
-        i += 1;
-    }
-}
+const playerRock = document.getElementById("rock");
+const playerPaper = document.getElementById("paper");
+const playerScissors = document.getElementById("scissors");
 
-var playerScore = 0;
-const playerWon = (`${playerSelection} beats ${computerSelection}, point to player!
-Current score: You: ${playerScore} Computer: ${computerScore}`);
+playerRock.addEventListener('click', () =>{
+    playerSelection = "rock";
+    playRound(playerSelection, computerSelection);
+});
 
-var computerScore = 0;
-const computerWon = (`${computerSelection} beats ${playerSelection}, point to computer!
-Current score: You: ${playerScore} Computer: ${computerScore}`);
+playerPaper.addEventListener('click', () =>{
+    playerSelection = "paper";
+    playRound(playerSelection, computerSelection);
+});
+
+playerScissors.addEventListener('click', () =>{
+    playerSelection = "scissors";
+    playRound(playerSelection, computerSelection);
+});
+
+let playerScore = 0;
+let computerScore = 0;
 
 function playRound() {
-    while (playerSelection === computerSelection) {
-        console.log("It's a tie! Go again.");
-        playerSelection = getPlayerSelection();
-        computerSelection = getComputerChoice();
-    }
-    if ((playerSelection === "rock" && computerSelection === "paper") ||
+    const playerWon =`${playerSelection} beats ${computerSelection}, point to player!
+    current score: you: ${playerScore} computer: ${computerScore}`;
+    const computerWon = `${computerSelection} beats ${playerSelection}, point to computer!
+    current score: you: ${playerScore} computer: ${computerScore}`;
+
+    if (playerSelection === computerSelection) {
+        console.log("it's a tie! go again.");
+    } else if((playerSelection === "rock" && computerSelection === "paper") ||
         (playerSelection === "paper" && computerSelection === "scissors") ||
         (playerSelection === "scissors" && computerSelection === "rock")) {
         computerScore += 1;
-        console.log(`${computerSelection} beats ${playerSelection}, point to computer!
-        Current score: You: ${playerScore} Computer: ${computerScore}`);
-        return;
+        console.log(computerWon);
     } else if ((playerSelection === "rock" && computerSelection === "scissors") ||
         (playerSelection === "paper" && computerSelection === "rock") ||
         (playerSelection === "scissors" && computerSelection === "paper")) {
             playerScore += 1;
-            console.log(`${playerSelection} beats ${computerSelection}, point to player!
-            Current score: You: ${playerScore} Computer: ${computerScore}`);
-            return;
-    }
-}
+            console.log(playerWon);
+    };
 
-function game() {
-    while ((playerScore || computerScore) < 5) {
-        playerSelection = getPlayerSelection();
-        computerSelection = getComputerChoice();
-        playRound(playerSelection, computerSelection);
-        if (playerScore === 5) {
-            console.log("Good job, you have beat the computer!");
-            break;
-        } else if (computerScore === 5) {
-            console.log("Tough luck next time, the computer beat you...");
-            break;
-        }
-    } 
-    playerScore = 0;
-    computerScore = 0;
-}
-
-console.log(game(playerSelection, computerSelection));
+    computerSelection = getComputerChoice();
+};
